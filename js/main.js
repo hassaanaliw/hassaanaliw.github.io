87a464a6f8b1bc628458566c98610bb05a4ec497
Hassaan Ali Wattoo <hassaanaliw@gmail.com>
2017-02-27 22:45:53 +0500

    Add Easter Egg

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var seq = "th"
+var input = ""
+window.addEventListener("keypress", function(e) {
+    input += String.fromCharCode(e.keyCode)
+
+    for (var i = 0; i < seq.length; i++) {
+        if (input[i] != seq[i] && input[i] != undefined) {
+            input = ""
+        }
+    }
+
+    if (input == seq) {
+        startGame();
+
+        input = ""
+    }
+})
+
+function startGame(){
+    var element = document.getElementById("boring");
+    element.classList.add("theta");
+
+    document.getElementById("header").style.display = 'none';
+    document.getElementById("theta").style.display = 'block';
+
+    var audio = new Audio('../assets/drama.mp3');
+    audio.play();
+
+
+
+}
