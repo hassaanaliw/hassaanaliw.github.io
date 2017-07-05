2ebabf45fedae78b3ee6535e4bc9d0f1539a8e4b
Hassaan Ali Wattoo <hassaanaliw@gmail.com>
2017-07-05 19:47:51 +0500

    Add song rand

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 console.log("Reading JSON");
 
-var test = "Want to listen to a song? How about <a href='{0}'>{1} by {2}</a>"
+var test = "Want to listen to a song? How about <strong><i>{0} by {1}</i> </strong>(<a href='{2}' target='_blank'>Spotify, </a><a href='{3}' target='_blank'>{4}</a>)"
 $.getJSON("website.json", function(json) {
     console.log(json); // this will show the info it in firebug console
     var tracks = json.playlists.website.tracks;
     console.log(tracks); 
     var song = tracks[Math.floor(Math.random()*tracks.length)];
     link = "https://open.spotify.com/track/"+  song.uri.split(":")[2];
+    if(song.video){
+        $(".song").html(test.f(song.name,song.artists[0],link,song.video,"Youtube"))
+    }
+    else{
+        $(".song").html(test.f(song.name,song.artists[0],link,song.music,"mp3"))
+    }
 
-    $(".song").html(test.f(link,song.name,song.artists[0]))
+    
 
 });
 
